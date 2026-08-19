c472165e6da8c98f6b14d7794cc78126074e81ea
imk97 <itmam97@gmail.com>
2026-08-19 21:51:49 +0800

    personal information creation

---
+++ subpages/profile/personal/personal.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Personal</title>
+</head>
+
+<body>
+    <form action="/action_page.php">
+        <fieldset>
+            <legend>Personal information:</legend>
+            <img src="img.jpg" alt="John" style="width:100%">
+            <br><br>
+            Full name:<br>
+            <input type="text" name="firstname" value="Mickey">
+            <br><br>
+            User name:<br>
+            <input type="text" name="lastname" value="Mouse">
+            <br><br>
+            Email:<br>
+            <input type="text" name="lastname" value="Mouse">
+            <br><br>
+            Phone Number:<br>
+            <input type="text" name="lastname" value="Mouse">
+            <br><br>
+            <input type="submit" value="Submit">
+        </fieldset>
+    </form>
+</body>
+
+</html>
