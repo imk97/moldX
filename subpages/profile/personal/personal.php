7eff5e6433cc809a853315228533a9a5d2c80b95
ItmamMdKhairi <itmam97@gmail.com>
2026-08-23 10:11:00 +0800

    phone region

--- subpages/profile/personal/personal.php
+++ subpages/profile/personal/personal.php
@@ -5,28 +5,64 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Personal</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/intl-tel-input@29.2.3/dist/css/intlTelInput.css">
+
+
+    <style>
+        fieldset {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+        }
+
+        img {
+            border-radius: 50%;
+        }
+
+        .img-container {
+            width: 80%;
+            margin: 20px auto;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+    </style>
 </head>
 
 <body>
+    <script src="https://cdn.jsdelivr.net/npm/intl-tel-input@29.2.3/dist/js/intlTelInput.min.js"></script>
+    <script>
+        const input = document.querySelector("#phone");
+        window.intlTelInput(input, {
+            loadUtils: () => import("https://cdn.jsdelivr.net/npm/intl-tel-input@29.2.3/dist/js/utils.js"),
+        });
+    </script>
+
     <form action="/action_page.php">
-        <fieldset>
-            <legend>Personal information:</legend>
-            <img src="img.jpg" alt="John" style="width:100%">
-            <br><br>
-            Full name:<br>
+        <!-- <fieldset> -->
+        <!-- <legend>Personal information:</legend> -->
+        <div class="img-container">
+            <img src="9439682.jpg" alt="John" style="width:50%">
+        </div>
+
+        <div class="container">
+            <label for="fullname">Full name</label>
             <input type="text" name="firstname" value="Mickey">
-            <br><br>
-            User name:<br>
+
+            <label for="uname">User name</label>
             <input type="text" name="lastname" value="Mouse">
-            <br><br>
-            Email:<br>
-            <input type="text" name="lastname" value="Mouse">
-            <br><br>
-            Phone Number:<br>
-            <input type="text" name="lastname" value="Mouse">
-            <br><br>
+
+            <label for="uname">Email</label>
+            <input type="email" name="email" value="Mouse@gmail.com">
+
+            <label for="phone">Phone Number</label>
+            <input type="tel" id="phone" name="phone" required>
+
             <input type="submit" value="Submit">
-        </fieldset>
+        </div>
+
+        <!-- </fieldset> -->
     </form>
 </body>
 
