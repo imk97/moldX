f73116841eb4fecf95630e6b6d2441ca4c9a1af7
imk97 <itmam97@gmail.com>
2026-09-26 23:11:32 +0800

    no cache for redirect to main file

--- login.php
+++ login.php
@@ -12,10 +12,15 @@ $_SESSION['fileDir']["sys_url"]="/assetx/";
 $_SESSION['fileDir']["sys_mainpage"]=$_SESSION['fileDir']["sys_url"]."/main_screen.php?f=0101000000000000000000000000000000000002";
 
 $_SESSION['db']["host"] = 'localhost';
-$_SESSION['db']["user"] = 'ajau';
-$_SESSION['db']["password"] = 'mousehitam';
+$_SESSION['db']["user"] = 'root';
+$_SESSION['db']["password"] = '';
 $_SESSION['db']["dbname"] = 'utmasset1';
 
-header( "Location: ".$_SESSION['fileDir']["sys_mainpage"]);
+// 1. Tell the browser explicitly NOT to store or cache this redirect
+header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
+header('Pragma: no-cache');
+header('Expires: Thu, 01 Jan 1970 00:00:00 GMT');
+
+header("Location: ".$_SESSION['fileDir']["sys_mainpage"], true, 301);
 exit();
 ?>
